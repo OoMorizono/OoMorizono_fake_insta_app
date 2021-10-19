b950128b009d72427044da6dc0bcaedae837612b
OoMorizono <x65f588d72sw76@gmail.com>
2021-10-19 14:16:49 +0900

    一覧画面作成

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -1,11 +1,25 @@
 <?php
 
-namespace App\Models;
+    namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Support\Facades\Storage;
 
-class Article extends Model
-{
-    use HasFactory;
-}
+    class Article extends Model
+    {
+        use HasFactory;
+
+        public function attachment()
+        {
+            return $this->hasOne(Attachment::class);
+        }
+        public function getImagePathAttribute()
+        {
+            return 'articles/' . $this->attachment->name;
+        }
+        public function getImageUrlAttribute()
+        {
+            return Storage::url($this->image_path);
+        }
+    }
